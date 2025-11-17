ed2f604659194da7618f543bb5ef834061b0bb8d
unigas <markus@samuelson.nu>
2025-11-17 10:40:20 +0100

    Changed to MAC address identification

--- custom_components/swissinno_ble/binary_sensor.py
+++ custom_components/swissinno_ble/binary_sensor.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 from datetime import datetime, timedelta
 
@@ -24,20 +23,21 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     """Set up SWISSINNO BLE binary sensors."""
     _LOGGER.info("SWISSINNO BLE: Registering Bluetooth scanner callback...")
 
-    sensors = {}
+    sensors: dict[str, SwissinnoTrapSensor] = {}
 
     @callback
     def detection_callback(service_info: BluetoothServiceInfoBleak, change):
         """Handle BLE advertisements."""
-        manufacturer_data = service_info.manufacturer_data
-        if MANUFACTURER_ID not in manufacturer_data:
+        man = service_info.manufacturer_data
+        if MANUFACTURER_ID not in man:
             return
 
-        frame = decode_frame(manufacturer_data[MANUFACTURER_ID])
+        frame = decode_frame(man[MANUFACTURER_ID])
         if not frame:
             return
 
-        trap_id = frame.trap_id
+        # Stable trap ID = BLE MAC without colons
+        trap_id = service_info.address.replace(":", "").lower()
         rssi = service_info.rssi
 
         _LOGGER.info(
@@ -45,67 +45,61 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
             f"RSSI={rssi} dBm | Battery={frame.battery_volts} V"
         )
 
-        # Update or create the binary sensor
         if trap_id in sensors:
             sensors[trap_id].update_state(frame.is_tripped)
         else:
-            sensor = SwissinnoTrapSensor(service_info.address, trap_id, frame.is_tripped)
-            sensors[trap_id] = sensor
-            async_add_entities([sensor], update_before_add=True)
+            entity = SwissinnoTrapSensor(service_info.address, trap_id, frame.is_tripped)
+            sensors[trap_id] = entity
+            async_add_entities([entity], update_before_add=True)
 
-        # Forward to battery + RSSI sensors if present
-        if DOMAIN in hass.data and "update_sensors" in hass.data[DOMAIN]:
+        # Route to battery + RSSI sensors if available
+        updater = hass.data.get(DOMAIN, {}).get("update_sensors")
+        if updater:
             hass.async_create_task(
-                hass.data[DOMAIN]["update_sensors"](
-                    trap_id,
-                    service_info.address,
-                    rssi,
-                    frame.battery_volts,
-                )
+                updater(trap_id, service_info.address, rssi, frame.battery_volts)
             )
 
     cancel_callback = async_register_callback(
         hass,
         detection_callback,
-        {},
+        {},  # no additional filter (we use manufacturer ID in code)
         BluetoothScanningMode.PASSIVE,
     )
+
     hass.bus.async_listen_once("homeassistant_stop", cancel_callback)
 
-    _LOGGER.info("SWISSINNO BLE: Bluetooth scanner callback registered successfully!")
-
+    _LOGGER.info("SWISSINNO BLE: Bluetooth scanner callback registered.")
+    
 
 class SwissinnoTrapSensor(BinarySensorEntity):
-    """Representation of a SWISSINNO BLE trap state."""
+    """Representation of a SWISSINNO BLE trap."""
 
     _attr_device_class = "problem"
 
-    def __init__(self, address: str, trap_id: str, is_tripped: bool):
-        _LOGGER.info(f"SWISSINNO BLE: Creating sensor for trap {trap_id}")
+    def __init__(self, address: str, trap_id: str, tripped: bool):
+        self._address = address
+        self._trap_id = trap_id
+        self._state = tripped
+        self._last_seen = datetime.utcnow()
 
         self._attr_name = f"SWISSINNO Trap {trap_id}"
         self._attr_unique_id = f"swissinno_trap_{trap_id}"
-        self._state = is_tripped
-        self._last_seen = datetime.utcnow()
 
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, trap_id)},
-            name=f"SWISSINNO Trap {trap_id}",
             manufacturer="SWISSINNO",
+            name=f"SWISSINNO Trap {trap_id}",
         )
 
     @property
-    def is_on(self):
-        """Device is ON when it is tripped."""
+    def is_on(self) -> bool:
         return self._state
 
     @property
     def available(self):
-        """Trap becomes unavailable if not updated recently."""
         return datetime.utcnow() - self._last_seen < LAST_SEEN_TIMEOUT
 
-    def update_state(self, is_tripped: bool):
-        """Update trap state."""
-        self._state = is_tripped
+    def update_state(self, tripped: bool):
+        self._state = tripped
         self._last_seen = datetime.utcnow()
         self.async_write_ha_state()
--- custom_components/swissinno_ble/sensor.py
+++ custom_components/swissinno_ble/sensor.py
@@ -1,18 +1,11 @@
 import logging
 from datetime import datetime, timedelta
 
-from homeassistant.components.bluetooth import (
-    BluetoothServiceInfoBleak,
-    async_register_callback,
-    BluetoothScanningMode,
-)
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, MANUFACTURER_ID
-from .decoder import decode_frame
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,25 +13,26 @@ LAST_SEEN_TIMEOUT = timedelta(minutes=30)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    """Set up BLE battery + RSSI sensors."""
+    """Prepare update callback for binary_sensor.py."""
     _LOGGER.info("SWISSINNO BLE: Initializing battery + RSSI sensors")
 
-    battery_sensors = {}
-    rssi_sensors = {}
+    battery_sensors: dict[str, SwissinnoBatterySensor] = {}
+    rssi_sensors: dict[str, SwissinnoRSSISensor] = {}
 
-    async def update_sensors(trap_id, address, rssi, battery_v):
-        """Update HA sensor entities after binary sensor detection."""
+    async def update_sensors(trap_id: str, address: str, rssi: int, battery_v: float):
+        # Battery
         if trap_id in battery_sensors:
             battery_sensors[trap_id].update_value(battery_v)
         else:
-            sensor = SwissinnoBatterySensor(address, trap_id, battery_v)
+            sensor = SwissinnoBatterySensor(trap_id, battery_v)
             battery_sensors[trap_id] = sensor
             async_add_entities([sensor])
 
+        # RSSI
         if trap_id in rssi_sensors:
             rssi_sensors[trap_id].update_value(rssi)
         else:
-            sensor = SwissinnoRSSISensor(address, trap_id, rssi)
+            sensor = SwissinnoRSSISensor(trap_id, rssi)
             rssi_sensors[trap_id] = sensor
             async_add_entities([sensor])
 
@@ -52,20 +46,23 @@ class SwissinnoBatterySensor(SensorEntity):
     device_class = "voltage"
     native_unit_of_measurement = "V"
 
-    def __init__(self, address, trap_id, battery_v):
+    def __init__(self, trap_id: str, battery_v: float):
+        self._trap_id = trap_id
+        self._value = battery_v
+        self._last_seen = datetime.utcnow()
+
         self._attr_name = f"SWISSINNO Trap {trap_id} Battery"
         self._attr_unique_id = f"swissinno_trap_{trap_id}_battery"
         self._attr_native_value = battery_v
-        self._last_seen = datetime.utcnow()
 
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, trap_id)},
-            manufacturer="SWISSINNO",
-            name=f"SWISSINNO Trap {trap_id}",
-        )
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, trap_id)},
+            "manufacturer": "SWISSINNO",
+            "name": f"SWISSINNO Trap {trap_id}",
+        }
 
-    def update_value(self, battery_v):
-        self._attr_native_value = battery_v
+    def update_value(self, value: float):
+        self._attr_native_value = value
         self._last_seen = datetime.utcnow()
         self.async_write_ha_state()
 
@@ -75,19 +72,22 @@ class SwissinnoRSSISensor(SensorEntity):
 
     native_unit_of_measurement = "dBm"
 
-    def __init__(self, address, trap_id, rssi):
+    def __init__(self, trap_id: str, rssi: int):
+        self._trap_id = trap_id
+        self._value = rssi
+        self._last_seen = datetime.utcnow()
+
         self._attr_name = f"SWISSINNO Trap {trap_id} RSSI"
         self._attr_unique_id = f"swissinno_trap_{trap_id}_rssi"
         self._attr_native_value = rssi
-        self._last_seen = datetime.utcnow()
 
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, trap_id)},
-            manufacturer="SWISSINNO",
-            name=f"SWISSINNO Trap {trap_id}",
-        )
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, trap_id)},
+            "manufacturer": "SWISSINNO",
+            "name": f"SWISSINNO Trap {trap_id}",
+        }
 
-    def update_value(self, rssi):
+    def update_value(self, rssi: int):
         self._attr_native_value = rssi
         self._last_seen = datetime.utcnow()
         self.async_write_ha_state()
