11d8d55f48530f77d43b4e411d18d59ad19f97c5
unigas <markus@samuelson.nu>
2026-08-06 11:42:43 +0200

    Satisfy Hassfest manifest validation

--- custom_components/swissinno_ble/__init__.py
+++ custom_components/swissinno_ble/__init__.py
@@ -2,11 +2,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the SWISSINNO BLE integration via YAML (legacy method)."""
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up SWISSINNO BLE integration."""
     await hass.config_entries.async_forward_entry_setups(
